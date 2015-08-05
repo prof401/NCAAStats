908cf2e1b67bc4d4c2ffa896e9f0d0556dfe51d6
Gerald Scheetz <bits@april1.net>
2015-08-05 00:29:15 -0600

    step

--- src/net/april1/calciostats/NCAA.java
+++ src/net/april1/calciostats/NCAA.java
@@ -1,12 +1,16 @@
 package net.april1.calciostats;
 
 import java.io.IOException;
+import java.net.MalformedURLException;
+import java.net.SocketTimeoutException;
 import java.util.HashSet;
 import java.util.Set;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
+import org.jsoup.HttpStatusException;
 import org.jsoup.Jsoup;
+import org.jsoup.UnsupportedMimeTypeException;
 import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
@@ -14,19 +18,20 @@ import org.jsoup.select.Elements;
 public class NCAA {
 
 	private static final String BASE_URL = "http://stats.ncaa.org";
-	private static final String TEAMS_URL = BASE_URL
-			+ "/team/inst_team_list?sport_code=WSO";
+	private static final String TEAMS_URL = BASE_URL + "/team/inst_team_list?sport_code=WSO";
 	private static final String PLAY_URL = BASE_URL + "/game/play_by_play/";
 
 	public void getGameData(String game) {
 		try {
-			Document doc = Jsoup.connect(PLAY_URL + game).get();
+			Document doc = getDocument(PLAY_URL + game);
 			Elements tableRows = doc.select("tr:has(td.smtext:matches(GOAL))");
-//			Elements tableRows = doc.select("tr:has(td.smtext)");
 			for (Element tableRow : tableRows) {
-				System.out.println(tableRow.toString());
+				System.out.print(tableRow.child(0).text());
+				System.out.print(' ');
+				System.out.println(tableRow.child(2).text());
+				System.out.println(tableRow.child(1).text());
+				System.out.println(tableRow.child(3).text());
 			}
-
 		} catch (IOException ioe) {
 			System.err.println("ERROR gettings game data " + game);
 			System.err.println("      " + ioe.getMessage());
@@ -34,7 +39,7 @@ public class NCAA {
 		}
 
 	}
-	
+
 	public Set<String> getGames() throws IOException {
 		Set<String> gameSet = new HashSet<String>();
 		for (String year : getYears()) {
@@ -53,13 +58,13 @@ public class NCAA {
 		final Pattern pattern = Pattern.compile("(\\d+)");
 		Set<String> gameSet = new HashSet<String>();
 		try {
-			Document doc = Jsoup.connect(BASE_URL + teamLink).get();
+			Document doc = getDocument(BASE_URL + teamLink);
 			Elements links = doc.select("a[href^=/game/index/]");
 			for (Element link : links) {
 				Matcher matcher = pattern.matcher(link.attr("href").toString());
 				matcher.find();
 				gameSet.add(matcher.group(0));
-				//gameSet.add(link.attr("href").toString());
+				// gameSet.add(link.attr("href").toString());
 			}
 		} catch (IOException ioe) {
 			System.err.println("ERROR gettings teamLink " + teamLink);
@@ -73,16 +78,13 @@ public class NCAA {
 	private Set<String> getTeamLink(String year, String division) {
 		Set<String> teamSet = new HashSet<String>();
 		try {
-			Document doc = Jsoup.connect(
-					TEAMS_URL + "&academic_year=" + year + "&division="
-							+ division).get();
+			Document doc = getDocument(TEAMS_URL + "&academic_year=" + year + "&division=" + division);
 			Elements links = doc.select("a[href^=/team/index/]");
 			for (Element link : links) {
 				teamSet.add(link.attr("href").toString());
 			}
 		} catch (IOException ioe) {
-			System.err.println("ERROR gettings year " + year + " division "
-					+ division);
+			System.err.println("ERROR gettings year " + year + " division " + division);
 			System.err.println("      " + ioe.getMessage());
 		}
 		return teamSet;
@@ -92,8 +94,7 @@ public class NCAA {
 		Set<String> divisionSet = new HashSet<String>();
 		final Pattern pattern = Pattern.compile("(\\d+)");
 		try {
-			Document doc = Jsoup.connect(TEAMS_URL + "&academic_year=" + year)
-					.get();
+			Document doc = getDocument(TEAMS_URL + "&academic_year=" + year);
 			Elements links = doc.select("a[href^=javascript:changeDivisions]");
 			for (Element link : links) {
 				Matcher matcher = pattern.matcher(link.attr("href").toString());
@@ -110,7 +111,7 @@ public class NCAA {
 	private Set<String> getYears() throws IOException {
 		final Pattern pattern = Pattern.compile("(\\d+)");
 		Set<String> yearSet = new HashSet<String>();
-		Document doc = Jsoup.connect(TEAMS_URL).get();
+		Document doc = getDocument(TEAMS_URL);
 		Elements links = doc.select("a[href^=javascript:changeYears]");
 		for (Element link : links) {
 			Matcher matcher = pattern.matcher(link.attr("href").toString());
@@ -120,25 +121,52 @@ public class NCAA {
 		return yearSet;
 	}
 
+	private Document getDocument(String url) throws IOException {
+		Document returnDoc = null;
+		int tries = 0;
+		do {
+			try {
+				returnDoc = Jsoup.connect(url).get();
+			} catch (MalformedURLException mue) {
+				System.err.println("Malformed URL " + url);
+				System.exit(-1);
+			} catch (HttpStatusException hse) {
+				System.err.println("Unexpected HTTP Status getting " + url);
+				System.exit(-1);
+			} catch (UnsupportedMimeTypeException ume) {
+				System.err.println("Unsupported MIME Type from " + url);
+				System.exit(-1);
+			} catch (SocketTimeoutException ste) {
+				System.err.println("Socket Timeout for " + url);
+				if (tries >= 3)
+					System.exit(-1);
+			} catch (IOException ioe) {
+				System.err.println("IO Exception for " + url);
+				throw ioe;
+			}
+		} while (returnDoc == null && tries++ < 3);
+		return returnDoc;
+	}
+
 	public static void main(String[] args) throws IOException {
-		
+
 		long start = System.currentTimeMillis();
 		NCAA ncaa = new NCAA();
 //		for (String game : ncaa.getGames()) {
-//			System.out.println("**"+game+"**");
+//			System.out.println("**" + game + "**");
 //			ncaa.getGameData(game);
 //		}
 		System.out.println("##### 4-3 #####");
-		ncaa.getGameData("350871"); //4-3 game
+		ncaa.getGameData("350871"); // 4-3 game
 		System.out.println("##### 0-0 #####");
-		ncaa.getGameData("3453824"); //0-0 tie
+		ncaa.getGameData("3453824"); // 0-0 tie
 		System.out.println("##### 3-3 #####");
 		ncaa.getGameData("3472071"); // 3-3 tie
 		System.out.println("##### 0-0 OT goal #####");
-		ncaa.getGameData("446670"); //0-0 regulation OT goal
+		ncaa.getGameData("446670"); // 0-0 regulation OT goal
 		System.out.println("##### 1-1 OT goal #####");
-		ncaa.getGameData("334030"); //1-1 regulation OT goal
-		System.out.print((System.currentTimeMillis() - start)/1000);
+		ncaa.getGameData("334030"); // 1-1 regulation OT goal
+		System.out.print((System.currentTimeMillis() - start) / 1000);
 		System.out.println(" DONE");
 	}
 }
